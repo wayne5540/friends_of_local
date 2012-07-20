b90b729cf8f792795b94c0b2712cb90bf64ac3f5
Airson <airson@server>
2012-07-20 15:24:38 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FriendsOfLocal::Application.config.secret_token = 'e55253c020484942c418e68b7624eb5ce5d23a44ae1d5e3315a4eebe6224941a1af33e800ee61c3afe64baf269cdbd2a9083bece13f39a4d13b6fb9ecda77ade'
