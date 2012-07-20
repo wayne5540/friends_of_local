62cab8407e43694b6bb8050db0525973b557f610
Airson <airson@server>
2012-07-20 15:29:54 +0800

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 FriendsOfLocal::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
+  get "static_pages/about"
+
+  get "static_pages/privacy"
+
+  get "static_pages/contact"
+
+  get "static_pages/report"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
